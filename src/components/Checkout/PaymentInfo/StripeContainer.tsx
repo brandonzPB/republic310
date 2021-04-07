736ee7250694bb6ab205e037f4dfd196850fe348
Brandon Zirulnikoff <zirbrandonweb@gmail.com>
2021-04-07 14:58:00 -0700

    Styling

--- src/components/Checkout/PaymentInfo/StripeContainer.tsx
+++ src/components/Checkout/PaymentInfo/StripeContainer.tsx
@@ -3,15 +3,20 @@ import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import PaymentForm from './PaymentForm';
 
+import './paymentForm.css';
+import './stripeContainer.css';
+
 const PUBLIC_KEY: string = 'pk_test_51IVJoHERKIXzxW4uSgEBYG8fI3hyfrYa198NWQZRxGnJpEiikE2Z0ktyVjsaQbsbIRFLS3kt1TMxpXVIsbB3laWR00xwGfHjDW';
 
 const stripeTestPromise: any = loadStripe(PUBLIC_KEY);
 
 const StripeContainer = () => {
   return (
-    <Elements stripe={stripeTestPromise}>
-      <PaymentForm />
-    </Elements>
+    <div id="stripe__container">
+      <Elements stripe={stripeTestPromise}>
+        <PaymentForm />
+      </Elements>
+    </div>
   )
 }
 
